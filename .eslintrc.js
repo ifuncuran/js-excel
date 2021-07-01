cfda0a29580c89c9c6c343fd80b3b2616ac1b823
ifuncuran <ifuncuran@gmail.com>
2021-07-01 22:20:29 +0300

    finish base framework functional

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
   extends: 'google',
   rules: {
     'object-curly-spacing': [2, 'always'],
+    'require-jsdoc': 0,
+    'operator-linebreak': 'off',
   },
 };
---
+++ src/components/table/Table.js
@@ -0,0 +1,9 @@
+import { ExcelComponent } from '@core/ExcelComponent';
+import { createTable } from './tableTemplate';
+
+export class Table extends ExcelComponent {
+  static className = 'table';
+  toHTML() {
+    return createTable();
+  }
+}
